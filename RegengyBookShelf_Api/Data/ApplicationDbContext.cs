2bfc5ce7b2cd9471c6a1697ce88ec92fc60b73b4
swati singh <swati952497@gmail.com>
2024-07-07 14:41:34 +0530

    BooksList View

--- RegengyBookShelf_Api/Data/ApplicationDbContext.cs
+++ RegengyBookShelf_Api/Data/ApplicationDbContext.cs
@@ -37,6 +37,14 @@ namespace RegengyBookShelf_Api.Data
                    Description = "A decade ago, the Marquess of Bourne was cast from society with nothing but his title. Now a partner in London’s most exclusive gaming hell, the cold, ruthless Bourne will do whatever it takes to regain his inheritance—including marrying perfect, proper Lady Penelope Marbury.\r\n\r\nA broken engagement and years of disappointing courtships have left Penelope with little interest in a quiet, comfortable marriage, and a longing for something more. How lucky that her new husband has access to such unexplored pleasures.\r\n\r\nBourne may be a prince of London’s underworld, but he vows to keep Penelope untouched by its wickedness—a challenge indeed as the lady discovers her own desires, and her willingness to wager anything for them... even her heart.",
                     ISBN = "9780062068521",
                     ImageUrl = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1327928208i/10428803.jpg"
+               }, new Books
+               {
+                Id = 2,
+                   Author = "Sarah MacLean",
+                   Title = "Ten Ways to Be Adored When Landing a Lord",
+                   Description = "Since being named on of London’s “Lords to Land” by a popular ladies’ magazine, Nicholas St. John has been relentlessly pursued by every matrimony-minded female in the ton.",
+                    ISBN = "9780061852060",
+                    ImageUrl = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1322231419i/7781699.jpg"
                });
         }
     }
