cc3236660834712b58a7ad51fd0813717f6a13c3
swati singh <swati952497@gmail.com>
2024-07-07 15:13:01 +0530

    AddBook Post

--- RegengyBookShelf_Web/Controllers/BooksController.cs
+++ RegengyBookShelf_Web/Controllers/BooksController.cs
@@ -3,12 +3,14 @@ using Newtonsoft.Json;
 using RegengyBookShelf_Web.Models;
 using RegengyBookShelf_Web.Models.Dtos;
 using RegengyBookShelf_Web.Services.IServices;
+using System.Text;
 
 namespace RegengyBookShelf_Web.Controllers
 {
     public class BooksController : Controller
     {
         private readonly IBooksService _booksService;
+        private static HttpClient _httpClient = new HttpClient();
 
         public BooksController(IBooksService booksService)
         {
@@ -29,6 +31,17 @@ namespace RegengyBookShelf_Web.Controllers
         public async Task<IActionResult> AddBook()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBook(BooksDto book) {
+            if (book != null) {
+                using (var content = new StringContent(JsonConvert.SerializeObject(book), Encoding.UTF8, "application/json"))
+                {
+                    await _httpClient.PostAsync("https://prod-88.eastus.logic.azure.com:443/workflows/a62aecd5e7324055b19e6358c86e6903/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=DgcgftknTUBOIgWhzvljEoqwOEnRSnuW9SqvYY2wZkM", content);
+                }
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
