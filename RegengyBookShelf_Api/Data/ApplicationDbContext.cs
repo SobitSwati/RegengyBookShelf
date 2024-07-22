05c216d2a24c319440fb788a9380b7ff0e853e71
swati singh <swati952497@gmail.com>
2024-07-22 08:23:16 +0530

    Updated tables in bookshelf

--- RegengyBookShelf_Api/Data/ApplicationDbContext.cs
+++ RegengyBookShelf_Api/Data/ApplicationDbContext.cs
@@ -36,7 +36,8 @@ namespace RegengyBookShelf_Api.Data
                    Title = "A Rogue by Any Other Name",
                    Description = "A decade ago, the Marquess of Bourne was cast from society with nothing but his title. Now a partner in London’s most exclusive gaming hell, the cold, ruthless Bourne will do whatever it takes to regain his inheritance—including marrying perfect, proper Lady Penelope Marbury.\r\n\r\nA broken engagement and years of disappointing courtships have left Penelope with little interest in a quiet, comfortable marriage, and a longing for something more. How lucky that her new husband has access to such unexplored pleasures.\r\n\r\nBourne may be a prince of London’s underworld, but he vows to keep Penelope untouched by its wickedness—a challenge indeed as the lady discovers her own desires, and her willingness to wager anything for them... even her heart.",
                     ISBN = "9780062068521",
-                    ImageUrl = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1327928208i/10428803.jpg"
+                    ImageUrl = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1327928208i/10428803.jpg",
+                    SeriesId = 1
                }, new Books
                {
                 Id = 2,
@@ -44,7 +45,8 @@ namespace RegengyBookShelf_Api.Data
                    Title = "Ten Ways to Be Adored When Landing a Lord",
                    Description = "Since being named on of London’s “Lords to Land” by a popular ladies’ magazine, Nicholas St. John has been relentlessly pursued by every matrimony-minded female in the ton.",
                     ISBN = "9780061852060",
-                    ImageUrl = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1322231419i/7781699.jpg"
+                    ImageUrl = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1322231419i/7781699.jpg",
+                   SeriesId = 1
                });
         }
     }
---
+++ RegengyBookShelf_Api/Migrations/20240721073110_AddNewDataToDbTables.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegengyBookShelf_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewDataToDbTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SeriesId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "SeriesId",
+                value: 1);
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Description", "ISBN", "ImageUrl", "SeriesId", "Title" },
+                values: new object[] { 2, "Sarah MacLean", "Since being named on of London’s “Lords to Land” by a popular ladies’ magazine, Nicholas St. John has been relentlessly pursued by every matrimony-minded female in the ton.", "9780061852060", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1322231419i/7781699.jpg", 1, "Ten Ways to Be Adored When Landing a Lord" });
+
+            migrationBuilder.UpdateData(
+                table: "Series",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 21, 13, 1, 7, 909, DateTimeKind.Local).AddTicks(8107));
+
+            migrationBuilder.UpdateData(
+                table: "Series",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 21, 13, 1, 7, 909, DateTimeKind.Local).AddTicks(8110));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_SeriesId",
+                table: "Books",
+                column: "SeriesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Series_SeriesId",
+                table: "Books",
+                column: "SeriesId",
+                principalTable: "Series",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Series_SeriesId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_SeriesId",
+                table: "Books");
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DropColumn(
+                name: "SeriesId",
+                table: "Books");
+
+            migrationBuilder.UpdateData(
+                table: "Series",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 2, 18, 18, 24, 730, DateTimeKind.Local).AddTicks(2293));
+
+            migrationBuilder.UpdateData(
+                table: "Series",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 2, 18, 18, 24, 730, DateTimeKind.Local).AddTicks(2296));
+        }
+    }
+}
